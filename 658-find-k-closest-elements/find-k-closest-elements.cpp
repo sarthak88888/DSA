58ff4b25e6442621cf3c93d283fabae23faf28c0
Sarthak Pundir <sarthakpundir0@gmail.com>
2026-01-14 23:15:22 +0530

    Time: 0 ms (100.00%) | Memory: 36.4 MB (50.89%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoptrmethod(vector<int>& arr, int k, int x) {
+        int l = 0, h = (int)arr.size() - 1;
+
+        while (h - l >= k) {
+            if (x - arr[l] > arr[h] - x) {
+                l++;
+            } else {
+                h--;
+            }
+        }
+
+        vector<int> ans;
+        for (int i = l; i <= h; i++) {
+            ans.push_back(arr[i]);
+        }
+
+        return ans;
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        return twoptrmethod(arr, k, x);
+    }
+};
