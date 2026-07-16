e4ebb629ff238524393080840735144ccc34e4bd
Sarthak Pundir <sarthakpundir0@gmail.com>
2026-07-16 12:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (9.17%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+
+        string first = strs[0];
+        string last = strs[strs.size()-1];
+
+        string ans = "";
+
+        for(int i =0;i<first.size();i++){
+            if(first[i] != last[i])
+            break;
+
+            ans+= first[i];
+        }
+        return ans;
+    }
+};
